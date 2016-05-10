511dd00040985185bdcb84693f275e2598645d38
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-05-11 08:03:30 +0900

    :hatching_chick:

---
+++ StatamicOverrideValetDriver.php
@@ -0,0 +1,85 @@
+<?php
+
+class StatamicOverrideValetDriver extends ValetDriver
+{
+    public function __construct()
+    {
+        $this->dir = '/html';
+    }
+
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return is_dir($sitePath . $this->dir . '/statamic');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (strpos($uri, $this->dir . '/site') === 0 && strpos($uri, $this->dir . '/site/themes') !== 0) {
+            return false;
+
+        } elseif (strpos($uri, $this->dir . '/local') === 0 || strpos($uri, $this->dir . '/statamic') === 0) {
+            return false;
+
+        } elseif (file_exists($staticFilePath = $sitePath . $this->dir . $uri)) {
+            return $staticFilePath;
+
+        } elseif (file_exists($staticFilePath = $sitePath . $this->dir . '/public' . $uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticPath = $sitePath . $this->dir . '/static' . $uri . '/index.html')) {
+            return $staticPath;
+        }
+
+        $_SERVER['SCRIPT_NAME'] = $this->dir . '/index.php';
+
+        if (strpos($_SERVER['REQUEST_URI'], $this->dir . '/index.php') === 0) {
+            $_SERVER['REQUEST_URI'] = substr($_SERVER['REQUEST_URI'], 10);
+        }
+
+        if ($uri === '') {
+            $uri = '/';
+        }
+
+        if ($uri === $this->dir . '/installer.php') {
+            return $sitePath . $this->dir . '/installer.php';
+        }
+
+        if (file_exists($indexPath = $sitePath . $this->dir . '/index.php')) {
+            return $indexPath;
+        }
+
+        if (file_exists($indexPath = $sitePath . $this->dir . '/public/index.php')) {
+            return $indexPath;
+        }
+    }
+}
